6c0bde5321023a42afabe723ed2d8e96c4dace4f
Daan Griffioen <81813141+DaanJGriffioen@users.noreply.github.com>
2024-02-07 16:49:02 +0100

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import re
+
+OutputFile = open("iPrice.txt", "a")
+OutputFile.writelines('\n'+ datetime.today().strftime('%Y-%m-%d') + '\n')
+url = ["https://www.refurbed.nl/p/iphone-12/", "https://www.refurbed.nl/p/iphone-12-pro/", "https://www.refurbed.nl/p/iphone-13/", "https://www.refurbed.nl/p/iphone-13-pro/"]
+name = ["iphone-12", "iphone-12-pro", "iphone-13", "iphone-13-pro"]
+
+for i in range(len(url)):
+  # Initialize minimal value array
+  minval = [999, 999, 999, 999, 999, 999]
+  # Print name of iphone
+  OutputFile.writelines("===" + name[i] + "===" + '\n')
+  #  Request url and find price information
+  data = requests.get(url[i])
+  soup = BeautifulSoup(data.text, 'html.parser')
+  price = soup.find_all('div', class_='mt-1.5 mr-2 text-sm text-right text-emphasize-03', string=True)
+  
+  # Format text
+  for p in range(len(price)):
+    text = price[p].get_text().strip("€ \n").replace(",",".")
+    text = int(float(text))
+    
+    #Get the 6 lowest values of the phone 
+    for j in range(6):
+      if text < minval[j]:
+        minval[j] = text
+        break
+  
+  # Output lowest prices to text file
+  for j in range(6):
+    text = minval[j]
+    OutputFile.writelines(str(text))
+    OutputFile.writelines(" ")
+  OutputFile.writelines('\n')
+
+OutputFile.close()
